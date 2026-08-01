9a042a03baa86108f0b4d088aa422a0f95e98a54
suneJQK <myloveforcfc96@gmail.com>
2026-08-01 16:26:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -167,8 +167,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 st.markdown(
-    '<div class="sub-header">Hệ thống luận giải thông minh ứng dụng AI Gemini'
-    " 2.5</div>",
+    '<div class="sub-header">Hệ thống luận giải thông minh ứng dụng AI </div>",
     unsafe_allow_html=True,
 )
 
